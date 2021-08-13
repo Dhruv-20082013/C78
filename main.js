4ad6daa84e0386ab8abf6d6668d3d0b76d1735ca
Dhruv-20082013 <85284596+Dhruv-20082013@users.noreply.github.com>
2021-08-13 09:59:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var first_name = "Dhruv";
+var last_name = "Rastogi";
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+
+var a = 200;
+var b = 200;
+var plus = Math.add = a+b;
+console.log(plus);
